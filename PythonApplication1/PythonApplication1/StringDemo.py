c7b6203a80c13d3c8667542deb76ed607884d3f7
XC <917563264@qq.com>
2024-05-29 16:08:20 +0800

    添加字符串的练习和数字的练习

---
+++ PythonApplication1/PythonApplication1/StringDemo.py
@@ -0,0 +1,22 @@
+﻿
+# encode()和decode()方法
+import encodings
+
+
+str = "风扇";
+print(str.encode())
+bytes1 = str.encode();
+print(bytes1.decode())
+
+#bytes类型
+name = bytes("111", encoding = "UTF-8")
+name2 = b""
+name3 = b"Cherry"
+print(name3)
+
+#删除空白
+hello = "  hello world   "
+print(hello)
+print(hello.lstrip())
+print(hello.rstrip())
+print(hello.strip())
---
+++ PythonApplication1/PythonApplication1/name_cases.py
@@ -0,0 +1,22 @@
+﻿
+# 练习
+# 2-3
+name = "Jack"
+print("你好" + name +"，今天怎么样?")
+
+# 2-4
+print(name.upper());
+print(name.lower());
+print(name.title());
+
+#2-5
+words = 'Arthur Schopenhauer once said "Wealth is like sea-water; the more we drink, the thirstier we become; and the same is true of fame."'
+print(words);
+
+#2-6
+famous_person = "Arthur Schopenhauer"
+message = "Wealth is like sea-water; the more we drink, the thirstier we become; and the same is true of fame."
+print(famous_person + ' once said "'+ message + '"');
+
+famous_person = "\tArthur Schopenhauer\n"
+print(famous_person.strip())
